ce9eaa024252155ccfc768effc4d090a24cde6aa
bizjaal1 <alan.bizjak@novartis.com>
2018-03-01 16:17:35 +0100

    string_to_html

---
+++ 003_string_to_html/01_stdout/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alan Bizjak"
+	tpl := `
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Hello Web!</title>
+</head>
+<body>
+<h1>` + name + `</h1> 
+</body>
+</html>
+`
+	fmt.Println(tpl)
+
+}
+
+/*
+go run main.go > index.html
+*/
